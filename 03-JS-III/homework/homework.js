3df85e1f68cb2c50d870616628a7f3ee57421a75
hernan3726 <hernan_carp93@hotmail.com>
2022-10-14 23:04:20 -0300

    first commit

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,7 +8,7 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
-   var contando = 10
+   var contando = 0
    while(contando <= 10) { 
     console.log( contando ) 
     contando ++ ; }   
@@ -34,8 +34,15 @@ function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-  
+  var nuevoArreglo = [];
+
+  for (let i = 0; i < array.length; i++) {
+    nuevoArreglo.push(Math.pow(array[i], 2))
+  }
+
+  return nuevoArreglo
 }
+  
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
